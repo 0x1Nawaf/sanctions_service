dfaea0c23710c11e5be4e562cd3c79e66134b2fc
0x1Nawaf <nawafaloufi@outlook.com>
2026-05-20 13:22:44 +0300

    records list filters

--- internal/handler/records.go
+++ internal/handler/records.go
@@ -29,18 +29,64 @@ func (h *RecordsHandler) List(w http.ResponseWriter, r *http.Request) {
 	}
 	offset := (page - 1) * perPage
 
+	q := r.URL.Query()
+	firstName := q.Get("first_name")
+	lastName := q.Get("last_name")
+	recordType := q.Get("record_type")
+	activeStatus := q.Get("active_status")
+	search := q.Get("search")
+
+	needsNameJoin := firstName != "" || lastName != "" || search != ""
+
+	var conditions []string
+	var args []interface{}
+
+	if recordType != "" {
+		conditions = append(conditions, "sr.record_type = ?")
+		args = append(args, recordType)
+	}
+	if activeStatus != "" {
+		conditions = append(conditions, "sr.active_status = ?")
+		args = append(args, activeStatus)
+	}
+	if firstName != "" {
+		conditions = append(conditions, "sn.first_name LIKE ?")
+		args = append(args, "%"+firstName+"%")
+	}
+	if lastName != "" {
+		conditions = append(conditions, "sn.surname LIKE ?")
+		args = append(args, "%"+lastName+"%")
+	}
+	if search != "" {
+		conditions = append(conditions, "(sn.first_name LIKE ? OR sn.surname LIKE ? OR sn.single_string_name LIKE ? OR sn.entity_name LIKE ?)")
+		args = append(args, "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%")
+	}
+
+	whereClause := ""
+	if len(conditions) > 0 {
+		whereClause = "WHERE " + conditions[0]
+		for _, c := range conditions[1:] {
+			whereClause += " AND " + c
+		}
+	}
+
+	fromClause := "FROM sanctions_records sr"
+	if needsNameJoin {
+		fromClause += " INNER JOIN sanctions_names sn ON sn.record_id = sr.id AND sn.name_type = 'Primary Name'"
+	}
+
+	countQuery := "SELECT COUNT(DISTINCT sr.id) " + fromClause + " " + whereClause
 	var total int
-	if err := h.db.QueryRow("SELECT COUNT(*) FROM sanctions_records").Scan(&total); err != nil {
+	if err := h.db.QueryRow(countQuery, args...).Scan(&total); err != nil {
 		writeError(w, http.StatusInternalServerError, "count query failed")
 		return
 	}
 
-	rows, err := h.db.Query(`
-		SELECT id, record_type, action, action_date, gender, active_status, deceased
-		FROM sanctions_records
-		ORDER BY id
-		LIMIT ? OFFSET ?
-	`, perPage, offset)
+	dataQuery := "SELECT DISTINCT sr.id, sr.record_type, sr.action, sr.action_date, sr.gender, sr.active_status, sr.deceased " +
+		fromClause + " " + whereClause + " ORDER BY sr.id LIMIT ? OFFSET ?"
+	dataArgs := append(args, perPage, offset)
+
+	rows, err := h.db.Query(dataQuery, dataArgs...)
 	if err != nil {
 		writeError(w, http.StatusInternalServerError, "query failed")
 		return
